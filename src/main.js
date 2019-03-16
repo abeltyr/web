4014f8587b4ea79127e91fcd87a3536f849429fd
avell21 <abeltyr@gmail.com>
2019-03-16 19:55:54 +0300

    added vuetfy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
 import App from "./App.vue";
 import router from "./router";
+import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "vuetify/dist/vuetify.css";
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   router,
